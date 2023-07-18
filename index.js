8c1eec606ba7f6c454a822196f9df54186992ae5
Wilson Kimunya <kimunya.wilson@gmail.com>
2023-07-18 18:44:02 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this for this
+function writeCards(names, gift) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${gift} gift!`);
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
